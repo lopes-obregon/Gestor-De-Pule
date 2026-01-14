57fe99a61b943bb0065ccdd207988e38f82546b3
Renan Obregon <lopesobregon.renan5@gmail.com>
2026-01-14 17:08:04 -0400

    Com Atualizador!

--- Gestor De Pule/Program.cs
+++ Gestor De Pule/Program.cs
@@ -1,5 +1,9 @@
 using Gestor_De_Pule.src.Persistencias;
 using Microsoft.EntityFrameworkCore;
+using NetSparkleUpdater;
+using NetSparkleUpdater.Enums;
+using NetSparkleUpdater.Interfaces;
+using NetSparkleUpdater.SignatureVerifiers;
 
 namespace Gestor_De_Pule
 {
@@ -12,6 +16,17 @@ namespace Gestor_De_Pule
         static void Main()
         {
             using var context = new DataBase();
+
+            var sparkle = new SparkleUpdater("https://raw.githubusercontent.com/lopes-obregon/Gestor-De-Pule/refs/heads/master/Gestor%20De%20Pule/appcast.xml",
+                new Ed25519Checker(SecurityMode.Strict, "/oSnntxisUdYkJG/Kh8Es6DTZ7gwTMRykGSEKJP8cTE="))
+            {
+                UIFactory = new NetSparkleUpdater.UI.WPF.UIFactory(),
+                RelaunchAfterUpdate= true,
+
+
+            };
+            sparkle.StartLoop(true);
+
             //context.Database.Migrate();
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
